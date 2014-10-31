9486f3f75514283ff363e21064c24d802c2ccd5f
armand <girier.armand@gmail.com>
2014-10-31 21:21:07 +0900

    mistype correction

--- character_sheet.js
+++ character_sheet.js
@@ -76,7 +76,7 @@ $(function(){
   function merge_objects(obj1,obj2){
     var obj3 = {};
     for (var attrname in obj1)if(obj1.hasOwnProperty(attrname)){ obj3[attrname] = obj1[attrname]; }
-    for (var attrname in obj2)if(obj1.hasOwnProperty(attrname)){ obj3[attrname] = obj2[attrname]; }
+    for (var attrname in obj2)if(obj2.hasOwnProperty(attrname)){ obj3[attrname] = obj2[attrname]; }
     return obj3;
   }
   
